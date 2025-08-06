89244c1e550f8e1808c97bef7de130b9439d39ce
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2025-08-06 22:15:03 +0800

    20250806-2214 add files

--- HFC/ch09/guard_log.c
+++ HFC/ch09/guard_log.c
@@ -12,6 +12,14 @@ char* now()
 
 int main()
 {
-    
+//    system("ls -l ~/");
+    char comment[80];
+    char cmd[120];
+
+    fgets(comment, 80, stdin);
+    sprintf(cmd, "echo '%s %s' >> reports.log", comment, now());
+
+    system(cmd);
+
     return 0;
 }
