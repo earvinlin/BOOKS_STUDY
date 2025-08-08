dd761a5415c45b2a0ba488d5c487645207b8e9c4
Earvin Lin <lin.earvin@gmail.com(OFF_VMDEBIAN64)>
2025-08-08 08:39:23 +0800

    20250808-0839 add files.

--- HFC/ch09/caller.c
+++ HFC/ch09/caller.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 int main()
 {
     char *my_env[] = {"JUICE=nothing", NULL};
+/*
     execle("diner_info", "diner_info", 4, NULL, my_env);
-    puts("Dude - the diner_info code must be busted");
+//    puts("Dude - the diner_info code must be busted");
+    puts(strerror(errno));
+*/
+    if (execle("diner_info", "diner_info", "4", NULL, my_env) == -1) {
+        fprintf(stderr, "Cannot run ipconfig: %s\n", strerror(errno));
+        return 1;
+    }
 
     return 0;
 }
