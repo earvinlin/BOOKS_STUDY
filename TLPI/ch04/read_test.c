9fccb5567657e6cd395e9b03b6ea0f2b2e64c242
Earvin Lin <lin.earvin@gmail.com(OFF_VMDEBIAN64)>
2025-08-15 08:39:48 +0800

    20250815-0839 add files.

--- TLPI/ch04/read_test.c
+++ TLPI/ch04/read_test.c
@@ -3,12 +3,18 @@
 #include "../tlpi-book/mylib/tlpi_hdr.h"
 #define MAX_READ 20
 
-char buffer[MAX_READ];
+char buffer[MAX_READ+1];
+ssize_t numRead;
 
 int main(int argc, char *argv[])
 {
-    if (read(STDIN_FILENO, buffer, MAX_READ) == -1)
+//    if (read(STDIN_FILENO, buffer, MAX_READ) == -1)
+//        errExit("read");
+    numRead = read(STDIN_FILENO, buffer, MAX_READ);
+    if (numRead == -1)
         errExit("read");
+
+    buffer[numRead] = '\0';
     printf("The input data was %s\n", buffer);
 
     return 0;
