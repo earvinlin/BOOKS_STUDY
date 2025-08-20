0ed16c7bb1265946040f4b8ef81d4fcbcdd8567c
Earvin Lin <lin.earvin@gmail.com(OFF_VMDEBIAN64)>
2025-08-20 19:03:03 +0800

    20250820-1902 add files.

--- CPrimer6th/ch16/preproc.c
+++ CPrimer6th/ch16/preproc.c
@@ -9,6 +9,7 @@ tive. - Oscar Wilde" /* a backslash continues a definition */
 #define FMT "X is %d.\n"
 
 #define MEAN(X, Y) (((X) + (Y)) / 2)
+#define SQUARE(X) X*X
 
 int main(void)
 {
@@ -19,7 +20,20 @@ int main(void)
     printf("%s\n", OW);
     printf("TWO: OW\n");
     
-    printf("2+3 = %d\n", MEAN(2, 3));
+    printf("55+32 = %d\n", MEAN(55, 32));       // ok
+    printf("55+32 = %f\n", MEAN(55, 32));       // 計算錯誤
+    printf("55.1+32 = %f\n", MEAN(55.1, 32));   // ok
+    printf("55.1+32 = %d\n", MEAN(55.1, 32));   // 計算錯誤
+
+    float s = MEAN(55,32);
+    printf("s(55+32) = %f\n", s);   // ok
+
+    
+
+    printf("%d\n", SQUARE( 3 ));
+    printf("%f\n", SQUARE( 3.1 ));
+    float z = SQUARE( 3.1 );
+    printf("%f\n", z);
 
     return 0;
 }
