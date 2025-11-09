d51175ed8b2da2beed02c85740b10c47ad754060
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2025-11-09 22:13:26 +0800

    20251109-2213 add files.

---
+++ TLPI/ch15/t_chown/t_chown.c
@@ -0,0 +1,45 @@
+#include <pwd.h>
+#include <grp.h>
+#include "../../tlpi-book/mylib/ugid_functions.h" /* Declarations of userIdFromName() and groupIdFromName() */
+#include "../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu use
+//#include "../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For mac-mini(m2)'s vmubuntu use
+
+int main(int argc, char *argv[])
+{
+    uid_t uid;
+    gid_t gid;
+    int j;
+    Boolean errFnd;
+
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s owner group [file...]\n"
+            " owner or group can be '-', "
+            "meaning leave unchanged\n", argv[0]);
+
+    if (strcmp(argv[1], "-") == 0) { /* "-" ==> don't change owner */
+        uid = -1;
+    } else { /* Turn user name into UID */
+        uid = userIdFromName(argv[1]);
+        if (uid == -1)
+            fatal("No such user (%s)", argv[1]);
+    }
+
+    if (strcmp(argv[2], "-") == 0) { /* "-" ==> don't change group */
+        gid = -1;
+    } else { /* Turn group name into GID */
+        gid = groupIdFromName(argv[2]);
+        if (gid == -1)
+            fatal("No group user (%s)", argv[1]);
+    }
+
+    /* Change ownership of all files named in remaining arguments */
+    errFnd = FALSE;
+    for (j = 3; j < argc; j++) {
+        if (chown(argv[j], uid, gid) == -1) {
+            errMsg("chown: %s", argv[j]);
+            errFnd = TRUE;
+        }
+    }
+
+    exit(errFnd ? EXIT_FAILURE : EXIT_SUCCESS);
+}
