2369fcade9c2aa97e26b0316de8fc1829e40d685
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2025-12-20 21:55:54 +0800

    20251220-2155 add files.

--- TLPI/ch22/catch_rtsigs/catch_rtsigs.c
+++ TLPI/ch22/catch_rtsigs/catch_rtsigs.c
@@ -1,7 +1,11 @@
 #define _GNU_SOURCE
 #include <string.h>
 #include <signal.h>
-#include "tlpi_hdr.h"
+#if defined(USE_MYLIB_INTEL)
+    #include "../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For linux(intel) use
+#else
+    #include "../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
+#endif
 
 static volatile int handlerSleepTime;
 static volatile int sigCnt = 0; /* Number of signals received */
