fa3d425440866f09b74577fc7cd44b606f0d91f8
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2026-01-21 08:57:36 +0800

    20260121-0857 add files.

--- TLPI/ch25/fork_stdio_buf/fork_stdio_buf.c
+++ TLPI/ch25/fork_stdio_buf/fork_stdio_buf.c
@@ -3,13 +3,15 @@
 #else
     #include "../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
 #endif
-int
-main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-printf("Hello world\n");
-write(STDOUT_FILENO, "Ciao\n", 5);
-if (fork() == -1)
-errExit("fork");
-/* Both child and parent continue execution here */
-exit(EXIT_SUCCESS);
+    printf("Hello world\n");
+    write(STDOUT_FILENO, "Ciao\n", 5);
+
+    if (fork() == -1)
+        errExit("fork");
+
+    /* Both child and parent continue execution here */
+    exit(EXIT_SUCCESS);
 }
