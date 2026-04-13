4f5274c8585615da626408468f039f1362c9436a
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-04-13 08:49:38 +0800

    20260413-0849 add files.

---
+++ TLPI/exams/ch46/svmsg_ls/svmsg_ls.c
@@ -0,0 +1,38 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/msg.h>
+#if defined(USE_MYLIB_INTEL)
+    #include "../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For linux(intel) use
+#else
+    #include "../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
+#endif
+
+int main(int argc, char *argv[])
+{
+    int maxind, ind, msqid;
+    struct msqid_ds ds;
+    struct msginfo msginfo;
+
+    /* Obtain size of kernel 'entries' array */
+
+    maxind = msgctl(0, MSG_INFO, (struct msqid_ds *) &msginfo);
+    if (maxind == -1)
+        errExit("msgctl-MSG_INFO");
+
+    printf("maxind: %d\n\n", maxind);
+    printf("index id key messages\n");
+
+    /* Retrieve and display information from each element of 'entries' array */
+
+    for (ind = 0; ind <= maxind; ind++) {
+        msqid = msgctl(ind, MSG_STAT, &ds);
+        if (msqid == -1) {
+            if (errno != EINVAL && errno != EACCES)
+                errMsg("msgctl-MSG_STAT"); /* Unexpected error */
+            continue; /* Ignore this item */
+        }
+        printf("%4d %8d 0x%08lx %7ld\n", ind, msqid, (unsigned long) ds.msg_perm.__key, 
+                (long) ds.msg_qnum);
+    }
+    exit(EXIT_SUCCESS);
+}
