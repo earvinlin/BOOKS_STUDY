9f25f67eb0fc908347b545c45d6b367ab2012523
Earvin Lin <earvin@VMDebianVIVO.idv>
2026-04-21 17:32:15 +0800

    20260421-1732 add files.

---
+++ TLPI/exams/ch47/binary_sems/binary_sems.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <sys/sem.h>
+#include "../../../tlpi-book/svsem/semun.h" /* Definition of semun union */
+#if defined(USE_MYLIB_INTEL)
+    #include "../../../tlpi-book/mylib-intel/tlpi_hdr.h"       // For linux(intel) use
+#else
+    #include "../../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
+#endif
+
+Boolean bsUseSemUndo = FALSE;
+Boolean bsRetryOnEintr = TRUE;
+
+/* Initialize semaphore to 1 (i.e., "available") */
+int initSemAvailable(int semId, int semNum) {
+    union semun arg;
+    arg.val = 1;
+
+    return semctl(semId, semNum, SETVAL, arg);
+}
+
+/* Initialize semaphore to 0 (i.e., "in use") */
+int initSemInUse(int semId, int semNum) {
+    union semun arg;
+    arg.val = 0;
+
+    return semctl(semId, semNum, SETVAL, arg);
+}
+
+/* Reserve semaphore (blocking), return 0 on success, or -1 with 'errno'
+set to EINTR if operation was interrupted by a signal handler */
+
+/* Reserve semaphore - decrement it by 1 */
+int reserveSem(int semId, int semNum) {
+    struct sembuf sops;
+    sops.sem_num = semNum;
+    sops.sem_op = -1;
+    sops.sem_flg = bsUseSemUndo ? SEM_UNDO : 0;
+
+    while (semop(semId, &sops, 1) == -1)
+        if (errno != EINTR || !bsRetryOnEintr)
+            return -1;
+
+    return 0;
+}
+
+/* Release semaphore - increment it by 1 */
+int releaseSem(int semId, int semNum) {
+    struct sembuf sops;
+    sops.sem_num = semNum;
+    sops.sem_op = 1;
+    sops.sem_flg = bsUseSemUndo ? SEM_UNDO : 0;
+
+    return semop(semId, &sops, 1);
+}
