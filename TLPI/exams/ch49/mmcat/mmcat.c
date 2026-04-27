ee9e4cd72ff7fc05b6ebc25916f1930b47c4bbb3
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-04-28 07:33:40 +0800

    20260428-0733 add files.

---
+++ TLPI/exams/ch49/mmcat/mmcat.c
@@ -0,0 +1,37 @@
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#if defined(USE_MYLIB_INTEL)
+    #include "../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For linux(intel) use
+#else
+    #include "../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
+#endif
+
+int main(int argc, char *argv[])
+{
+    char *addr;
+    int fd;
+    struct stat sb;
+
+    if (argc != 2 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s file\n", argv[0]);
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+        errExit("open");
+
+    /* Obtain the size of the file and use it to specify the size of
+        the mapping and the size of the buffer to be written */
+
+    if (fstat(fd, &sb) == -1)
+        errExit("fstat");
+    
+    addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED)
+        errExit("mmap");
+
+    if (write(STDOUT_FILENO, addr, sb.st_size) != sb.st_size)
+        fatal("partial/failed write");
+
+    exit(EXIT_SUCCESS);
+}
