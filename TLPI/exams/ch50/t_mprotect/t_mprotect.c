355c1cd0db9df41b937219380475ee8c81a38176
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2026-05-04 21:03:31 +0800

    20260504-2103 add files.

---
+++ TLPI/exams/ch50/t_mprotect/t_mprotect.c
@@ -0,0 +1,42 @@
+//#define _BSD_SOURCE /* Get MAP_ANONYMOUS definition from <sys/mman.h> */
+#define _DEFAULT_SOURCE
+
+#include <sys/mman.h>
+#if defined(USE_MYLIB_INTEL)
+    #include "../../../tlpi-book/mylib-intel/tlpi_hdr.h"       // For linux(intel) use
+#else
+    #include "../../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
+#endif
+#define LEN (1024 * 1024)
+#define SHELL_FMT "cat /proc/%ld/maps | grep zero"
+#define CMD_SIZE (sizeof(SHELL_FMT) + 20)
+
+/* Allow extra space for integer string */
+
+int main(int argc, char *argv[])
+{
+    char cmd[CMD_SIZE];
+    char *addr;
+
+    /* Create an anonymous mapping with all access denied */
+
+    addr = mmap(NULL, LEN, PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (addr == MAP_FAILED)
+        errExit("mmap");
+
+    /* Display line from /proc/self/maps corresponding to mapping */
+
+    printf("Before mprotect()\n");
+    snprintf(cmd, CMD_SIZE, SHELL_FMT, (long) getpid());
+    system(cmd);
+
+    /* Change protection on memory to allow read and write access */
+
+    if (mprotect(addr, LEN, PROT_READ | PROT_WRITE) == -1)
+        errExit("mprotect");
+
+    printf("After mprotect()\n");
+    system(cmd); /* Review protection via /proc/self/maps */
+
+    exit(EXIT_SUCCESS);
+}
