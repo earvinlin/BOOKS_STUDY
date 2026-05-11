f9be820d04ccadbf9ea6cb0053311cb7f96d5252
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2026-05-11 20:06:33 +0800

    20260511-2006 add files.

--- TLPI/exams/ch52/mq_notify_thread/mq_notify_thread.c
+++ TLPI/exams/ch52/mq_notify_thread/mq_notify_thread.c
@@ -12,6 +12,12 @@
 static void notifySetup(mqd_t *mqdp);
 
 /* Thread notification function */
+/**
+ * 由核心自動建立的執行緒進入此函式。
+ * 先呼叫 mq_getattr() 取得佇列屬性，配置 buffer。
+ * 再次呼叫 notifySetup() → 因為 POSIX 規範要求通知只會觸發一次，必須重新註冊。
+ * 使用 mq_receive() 讀取所有訊息並印出。
+ */
 static void threadFunc(union sigval sv) {
     ssize_t numRead;
     mqd_t *mqdp;
@@ -37,6 +43,13 @@ static void threadFunc(union sigval sv) {
     pthread_exit(NULL);
 }
 
+/**
+ * 呼叫 notifySetup()，在裡面建立 struct sigevent 並指定：
+ * sev.sigev_notify = SIGEV_THREAD;
+ * sev.sigev_notify_function = threadFunc;
+ * sev.sigev_value.sival_ptr = mqdp;
+ * 這表示：當佇列有新訊息到達時，核心會建立一個新執行緒，並呼叫 threadFunc()，同時把 mqdp 傳入。
+ */
 static void notifySetup(mqd_t *mqdp) {
     struct sigevent sev;
     sev.sigev_notify = SIGEV_THREAD; /* Notify via thread */
@@ -56,9 +69,12 @@ int main(int argc, char *argv[])
     if (argc != 2 || strcmp(argv[1], "--help") == 0)
         usageErr("%s mq-name\n", argv[0]);
 
+    // 開啟佇列
     mqd = mq_open(argv[1], O_RDONLY | O_NONBLOCK);
     if (mqd == (mqd_t) -1)
         errExit("mq_open");
+    
+    // 設定通知
     notifySetup(&mqd);
     
     pause(); /* Wait for notifications via thread function */
