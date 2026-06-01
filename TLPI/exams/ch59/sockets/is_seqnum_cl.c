d32105f6640a4c3b56981a88937b4a55b08dcc14
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2026-06-01 08:51:16 +0800

    20260601-0851 add files.

--- TLPI/exams/ch59/sockets/is_seqnum_cl.c
+++ TLPI/exams/ch59/sockets/is_seqnum_cl.c
@@ -10,20 +10,22 @@ int main(int argc, char *argv[])
     struct addrinfo hints;
     struct addrinfo *result, *rp;
 
+    // 2. 參數檢查
     if (argc < 2 || strcmp(argv[1], "--help") == 0)
         usageErr("%s server-host [sequence-len]\n", argv[0]);
 
     /* Call getaddrinfo() to obtain a list of addresses that
         we can try connecting to */
 
+    // 3. 取得伺服器位址
     memset(&hints, 0, sizeof(struct addrinfo));
 
     hints.ai_canonname = NULL;
     hints.ai_addr = NULL;
     hints.ai_next = NULL;
     hints.ai_family = AF_UNSPEC; /* Allows IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_socktype = SOCK_STREAM;    // TCP
+    hints.ai_flags = AI_NUMERICSERV;    // 服務名稱必須是數字
 
     if (getaddrinfo(argv[1], PORT_NUM, &hints, &result) != 0)
         errExit("getaddrinfo");
@@ -31,6 +33,7 @@ int main(int argc, char *argv[])
     /* Walk through returned list until we find an address structure
         that can be used to successfully connect a socket */
 
+    // 4. 建立並連線 socket
     for (rp = result; rp != NULL; rp = rp->ai_next) {
         cfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 
@@ -52,6 +55,7 @@ int main(int argc, char *argv[])
 
     /* Send requested sequence length, with terminating newline */
 
+    // 5. 送出請求
     reqLenStr = (argc > 2) ? argv[2] : "1";
     if (write(cfd, reqLenStr, strlen(reqLenStr)) != strlen(reqLenStr))
         fatal("Partial/failed write (reqLenStr)");
@@ -61,6 +65,7 @@ int main(int argc, char *argv[])
 
     /* Read and display sequence number returned by server */
 
+    // 6. 讀取伺服器回應
     numRead = readLine(cfd, seqNumStr, INT_LEN);
     if (numRead == -1)
         errExit("readLine");
