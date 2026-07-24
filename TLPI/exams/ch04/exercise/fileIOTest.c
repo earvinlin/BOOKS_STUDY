a39c90b89a5acf26ebf63bdadcab54bef9349397
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-07-24 10:50:49 +0800

    20260724-1050 add files.

--- TLPI/exams/ch04/exercise/fileIOTest.c
+++ TLPI/exams/ch04/exercise/fileIOTest.c
@@ -7,14 +7,52 @@
 #else
     #include "../../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
 #endif
+#include <libgen.h> // 使用 basename() 必須引入此標頭檔
+#include <string.h> // 使用 strrchr()
 
 #define MAX_READ 20
 
+char * getProgName(char *s) {
+    // 尋找字串中最後一個 '/'
+    char *progName = strrchr(s, '/');
+
+    if (progName != NULL) {
+        progName++; // 跳過 '/' 本身，指向檔名開頭
+    } else {
+        progName = s; // 如果路徑中沒有 '/'，直接使用 argv[0]
+    }
+//    printf("僅取程式名稱 : %s\n", progName);
+    return progName;
+}
+
 int main(int argc, char *argv[]) {
+    int ap;
     char buffer[MAX_READ];
 
+    /* 
+        argv的資料結構本質上是指向陣列的指標
+        ex: ./myprog -v input.txt 100
+        argv[0] ""./myprog" 程式本身的名稱/路徑（永遠是第一個參數）
+        argv[1] "-v"        使用者傳入的第 1 個參數
+        argv[2] "input.txt" 使用者傳入的第 2 個參數
+        argv[3] "100"       使用者傳入的第 3 個參數 (注意：類型依然是字串)
+        argv[4] NULL        C 語言標準規定，陣列最後一格永遠是 NULL 指針
+    */
+
+    printf("The argc value is %d\n", argc);
+    if (argc != 3) {
+        // 使用 basename 取得不帶路徑的純檔名
+        char *prog_name = basename(argv[0]);
+        printf("parameter error, program is %s\n", argv[0]);
+        printf("parameter error, program is %s\n", prog_name);
+
+        char *s = argv[0];
+        printf("=== %s ===\n", getProgName(s));
+        exit(1);
+    }
+    
     // 建立新檔案，並設定權限為 0644 (所有者可讀寫，其他人唯讀)
-    int fd = open("fileIOTest.txt", O_WRONLY | O_CREAT | O_TRUNC, 
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 
         S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
     if (fd == -1) {
@@ -23,6 +61,25 @@ int main(int argc, char *argv[]) {
     }
     printf("成功建立檔案，檔案描述符(FD)為：%d\n", fd);
 
+    char * op = argv[2];
+    switch (*op) {
+        case 'r':
+        case 'R':
+            printf("Input r/R value is %c\n", *op);
+            break;
+
+        case 'w':
+        case 'W':
+            printf("Input w/W value is %c\n", *op);
+            break;
+
+        default:
+            printf("Unknown command line : %c\n", *op);
+            break;
+    }
+  
+
+/*
     // 呼叫 write 寫入檔案
     char text[10] ="work work";
 //    const char *text = "test test\n";
@@ -39,7 +96,7 @@ int main(int argc, char *argv[]) {
     } else {
         printf("成功寫入 %zd 位元組到 test_write.txt\n", bytes_written);
     }
-
+*/
     close(fd);  // 關閉檔案
 
     return 0;
