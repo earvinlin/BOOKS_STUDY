690fa13562f6e552a1b82638ae90bf5d5821bf07
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-08-18 08:09:24 +0800

    20260818-0809 add files.

--- TLPI/exams/ch06/exercise/e6-2.c
+++ TLPI/exams/ch06/exercise/e6-2.c
@@ -2,21 +2,41 @@
  * 設計一個程式，觀察當使用longjmp（）函式跳轉到一個已經返回的函式時會發生什麼事？
  */
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include "ex5-7.h"
 #if defined(USE_MYLIB_INTEL)
     #include "../../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For linux(intel) use
 #else
     #include "../../../tlpi-book/mylib/tlpi_hdr.h"         // For macnb's vmubuntu(arm) use
 #endif
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf env;
+
+void do_something() {
+    printf("進入 do_something 函式\n");
+    if (setjmp(env) == 0) {
+        printf("setjmp 設置完成，準備離開 do_something 函式\n");
+    } else {
+        // 當 longjmp 跳回這裡時，do_something 的 Stack Frame 早就已經失效了！
+        printf("成功跳回 do_something！(但此處 Stack 已經壞掉)\n");
+    }
+}
 
 int main(int argc, char *argv[])
 {
+do_something(); // 呼叫函式，會在內部呼叫 setjmp 後 return 結束
+
+    printf("返回 main 函式，準備呼叫 longjmp 跳回已結束的函式...\n");
+    
+    // 試圖跳回已經 return 的 do_something 函式中
+    longjmp(env, 1);
+
     return 0;
 }
