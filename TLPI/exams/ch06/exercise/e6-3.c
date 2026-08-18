2745bfa4341a850e95f7168495bd4ac451db0b3c
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-08-18 17:59:20 +0800

    20260818-1759 add file.

--- TLPI/exams/ch06/exercise/e6-3.c
+++ TLPI/exams/ch06/exercise/e6-3.c
@@ -3,13 +3,13 @@
  * unsetenv(函式。此處的unsetenv()函式應檢查是否對環境變數進行了多次定義，如果是多
  * 次定義則將移除對該變數的全部定義(glibc版本的unsetenv()函式實作了這一功能)。
  */
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
-#include <unistd.h>
+//#include <sys/stat.h>
+//#include <sys/types.h>
+//#include <sys/uio.h>
+//#include <fcntl.h>
+//#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+//#include <string.h>
 #include <errno.h>
 #if defined(USE_MYLIB_INTEL)
     #include "../../../tlpi-book/mylib-intel/tlpi_hdr.h"   // For linux(intel) use
@@ -20,7 +20,20 @@
 
 int main(int argc, char *argv[])
 {
+    /** char *getenv(const char *name);  **/
+    // 取得 HOME 環境變數
+    char *home = getenv("HOME");
+    if (home != NULL) {
+        printf("HOME path: %s\n", home);
+    } else {
+        printf("HOME variable is not set.\n");
+    }
 
+    // 查詢不存在的變數
+    char *dummy = getenv("NON_EXISTENT_VAR");
+    if (dummy == NULL) {
+        printf("NON_EXISTENT_VAR does not exist.\n");
+    }    
 
     return 0;
 }
