830f9f972ea2ec7cc644c9f80082c1ba8cff6d88
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-08-19 15:09:36 +0800

    20260819-1509 add file.

--- TLPI/exams/ch07/exercise/e7-1.c
+++ TLPI/exams/ch07/exercise/e7-1.c
@@ -1,8 +1,7 @@
 /**
- * 修改列表7-1的程式(free_and_sbrk.c)，在每次執行malloc()之後，印出目前的
- * program break值。指定一個小的配置區塊來執行程式。這將能展示malloc()在每
- * 次呼明時不會用sbrk()調整program break，而是定期分配大塊的記憶體，並每次 
- * 傳回一小片記憶體給呼叫者。
+ * 修改列表7-1的程式(free_and_sbrk.c)，在每次執行malloc()之後，印出目前的program break值。指
+ * 定一個小的配置區塊來執行程式。這將能展示malloc()在每次呼明時不會用sbrk()調整program break ，
+ * 而是定期分配大塊的記憶體，並每次傳回一小片記憶體給呼叫者。
  */
 #include <stdlib.h>
 //#include <string.h>
