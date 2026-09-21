d5c9e15f66c1c5cc8a64642e749921d97be72538
earvinlin.02(macmini-vmubuntu) <earvinlin.02@gmail.com(macmini-vmubuntu)>
2026-09-21 08:34:48 +0800

    20260921-0832 add file.

--- TLPI/exams/ch10/exercise/e10-1.c
+++ TLPI/exams/ch10/exercise/e10-1.c
@@ -20,7 +20,7 @@
 int main(int argc, char *argv[])
 {
 
-    /*
+/*
     1. times() 的情況
         已知 sysconf(_SC_CLK_TCK) = 100
         表示：1 秒 = 100 ticks
@@ -46,13 +46,9 @@ int main(int argc, char *argv[])
             若：clock_t 為 N 位元，頻率為 F ticks/sec
             則完整循環時間：T = 2^N / F
         ．times()
-            T = 2^32 / 100
-            = 42949672.96 秒
-            ≈ 497 天
+            T = 2^32 / 100 = 42949672.96 秒 ≈ 497 天
         ．clock()
-            T = 2^32 / 1000000
-            = 4294.967296 秒
-            ≈ 71.6 分鐘
+            T = 2^32 / 1000000 = 4294.967296 秒 ≈ 71.6 分鐘
             因此答案為：
             ===========================================
             函式        Tick Rate           32-bit 週期
@@ -60,10 +56,9 @@ int main(int argc, char *argv[])
             times()     100 ticks/s         約 497 天
             clock()     1,000,000 ticks/s   約 71.6 分鐘
             ※ 這也是為什麼早期使用 32 位元 clock_t 時，clock() 
-               很容易在長時間執行程式中發生 wrap-around，而 
-               times() 則通常要一年多才會遇到一次。
+              很容易在長時間執行程式中發生 wrap-around，而 
+              times() 則通常要一年多才會遇到一次。
+*/
 
-
-    */ 
     return 0;
 }
